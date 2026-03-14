bdc5971f4bb847907f483c71f5c97951be52c05c
gueverstarstone <josephmungai028@gmail.com>
2026-03-14 03:48:43 +0300

    update frontend url

--- server/app.py
+++ server/app.py
@@ -31,7 +31,7 @@ def create_app():
     app.config.from_object(Config)
 
     # Enable CORS
-    CORS(app, origins=["https://campuspulse-frontend.netlify.app"])
+    CORS(app, origins=["https://univibez.netlify.app/"])
 
     # Initialize db, migrations, JWT
     db.init_app(app)
